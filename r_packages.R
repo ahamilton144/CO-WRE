4a2fb5e2b783ed630259bd9a0c787beeb48141c5
Andrew Hamilton <ahamilton144@users.noreply.github.com>
2017-01-26 11:13:39 -0500

    Add files via upload
    try install again

--- r_packages.R
+++ r_packages.R
@@ -1,4 +1,4 @@
 library(devtools)
 devtools::install_github("rstudio/shinythemes",force=T)
-#devtools::install_github("igraph/rigraph",force=T)
-install.packages('igraph')
+devtools::install_github("igraph/rigraph",force=T)
+#install.packages('igraph')
