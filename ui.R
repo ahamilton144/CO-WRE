a44c6eb9cb65cb15562d32a60a39298107fa9ea0
Andrew Hamilton <ahamilton144@users.noreply.github.com>
2017-01-30 12:53:14 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,3 +1,18 @@
+library(shiny)
+library(ggplot2)
+# library(devtools)
+# devtools::install_github("rstudio/shinythemes",force=T)
+#source('r_packages.R')
+# devtools::install_github("gaborcsardi/pkgconfig")
+# devtools::install_github("igraph/rigraph")
+#install.packages('igraph')
+#library(shinythemes)
+library(igraph)
+
+library(sp)  
+library(rgdal)
+library(leaflet)
+
 ui <- fluidPage(
   #theme = shinytheme('spacelab'),
   titlePanel('CO Water Right Explorer'),
