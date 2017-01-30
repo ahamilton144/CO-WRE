c58b0380ebc96d8fbcf742bbdfb28ca7c9e4a887
Andrew Hamilton <ahamilton144@users.noreply.github.com>
2017-01-30 12:43:40 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,21 @@
+# ui <- fluidPage(
+#   theme = shinytheme('spacelab'),
+#   titlePanel('CO Water Right Explorer'),
+#   sidebarLayout(
+#     sidebarPanel(  
+#       tabsetPanel(
+#         tabPanel('Introduction', id='tabIntro',
+#                  'TEST'
+#         ), 
+#         tabPanel('Instructions', id='tabInstr',
+#                  'write here too!')
+#       )
+#     ),
+#     mainPanel(
+#       textOutput('txt'),  # Output text (only if no data)
+#       h4('Full Dataset, Master'),
+#       #      textOutput('txtNoData'),  # Output text before data selection
+#       leafletOutput('map1')   # Output plot full dataset (only if data))
+#     )
+#   )
+# )
